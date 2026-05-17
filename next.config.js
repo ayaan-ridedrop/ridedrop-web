fc77df9586bf8ea836206ec1b6bc80b1cc009680
ridedrop <80923083+ayaan-ridedrop@users.noreply.github.com>
2026-05-17 21:48:24 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  onDemandEntries: {
+    maxInactiveAge: 60 * 60 * 1000,
+    pagesBufferLength: 5,
+  },
   images: {
     remotePatterns: [
       // Allow Supabase Storage images once you set the bucket up
